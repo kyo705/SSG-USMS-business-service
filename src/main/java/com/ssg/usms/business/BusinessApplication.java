fff3062fa5e7f1af709ac8ff81ebee99c40dbec4
kyo705 <89891704+kyo705@users.noreply.github.com>
2024-01-09 17:04:13 +0900

    Update BusinessApplication.java

--- src/main/java/com/ssg/usms/business/BusinessApplication.java
+++ src/main/java/com/ssg/usms/business/BusinessApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BusinessApplication {
-
+	//TEST
 	public static void main(String[] args) {
 		SpringApplication.run(BusinessApplication.class, args);
 	}
