73bd94ae99ae9204c91d0c8c153ea79e2fb9edb2
aksenaksen <ans109905@naver.com>
2024-01-13 18:21:00 +0900

    회원가입 기능 구현

--- src/main/java/com/ssg/usms/business/constant/CustomStatusCode.java
+++ src/main/java/com/ssg/usms/business/constant/CustomStatusCode.java
@@ -2,6 +2,16 @@ package com.ssg.usms.business.constant;
 
 public class CustomStatusCode {
 
+    public static final int NOT_ALLOWED_ID_FORM = 601;
+    public static final int NOT_ALLOWED_PASSWORD_FORM = 602;
+    public static final int NOT_ALLOWED_EMAIL_FORM = 603;
+    public static final int NOT_ALLOWED_KEY = 604;
+    public static final int ALREADY_EXIST_ID = 620;
+    public static final int ALREADY_EXIST_PHONE_NUM = 621;
+
+
+
+
     public static final int NOT_MATCHING_STREAM_PROTOCOL_AND_FILE_FORMAT = 628;
     public static final int NOT_ALLOWED_STREAM_PROTOCOL = 627;
     public static final int NOT_OWNED_STREAM_KEY = 626;
