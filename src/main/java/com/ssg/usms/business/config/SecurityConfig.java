4942fd0f792a287b9b27dd85907d955158c0a07a
aksenaksen <ans109905@naver.com>
2024-01-18 17:41:32 +0900

    [UPDATE] 회원가입 정보 수정

--- src/main/java/com/ssg/usms/business/config/SecurityConfig.java
+++ src/main/java/com/ssg/usms/business/config/SecurityConfig.java
@@ -1,4 +1,16 @@
 package com.ssg.usms.business.config;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
 }
