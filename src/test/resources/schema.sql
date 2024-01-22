2f695c643be0824bdc19f0914bb2f3b74b235570
kyo705 <mission0705@naver.com>
2024-01-22 11:04:47 +0900

    [FIX] SCHEMA 추가
    Entity 수정 및 테스트 코드에 스키마 추가

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,4 +1,17 @@
+DROP TABLE usms_user IF EXISTS;
 DROP TABLE store IF EXISTS;
+
+CREATE TABLE usms_user (
+       id INT AUTO_INCREMENT PRIMARY KEY,
+       username VARCHAR(255) NOT NULL,
+       password VARCHAR(255),
+       person_name VARCHAR(255),
+       phone_number VARCHAR(255) NOT NULL UNIQUE,
+       email VARCHAR(255) NOT NULL,
+       security_state VARCHAR(255) DEFAULT 'BASIC',
+       is_lock BOOLEAN DEFAULT false,
+       role VARCHAR(255) DEFAULT 'ROLE_STORE_OWNER'
+);
 
 CREATE TABLE store (
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
