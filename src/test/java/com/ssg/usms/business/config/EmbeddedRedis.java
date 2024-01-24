0a3d0e217d353f2afe24fbe57c6dacdf587b259f
aksenaksen <ans109905@naver.com>
2024-01-24 20:28:42 +0900

    [UPDATE] EmbeddedRedis.java수정

--- src/test/java/com/ssg/usms/business/config/EmbeddedRedis.java
+++ src/test/java/com/ssg/usms/business/config/EmbeddedRedis.java
@@ -3,6 +3,7 @@ package com.ssg.usms.business.config;
 import org.junit.jupiter.api.AfterEach;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.util.SocketUtils;
 import redis.embedded.RedisServer;
 
 import javax.annotation.PostConstruct;
@@ -14,7 +15,7 @@ public class EmbeddedRedis {
     private RedisServer redisServer;
 
     public EmbeddedRedis(RedisProperties redisProperties) {
-        this.redisServer = new RedisServer(redisProperties.getPort());
+        this.redisServer = new RedisServer(SocketUtils.findAvailableTcpPort());
     }
 
     @PostConstruct
