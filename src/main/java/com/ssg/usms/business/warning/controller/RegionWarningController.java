4f3fb3de36ee934c8f0866cda5253d0eb105c47f
kyo705 <mission0705@naver.com>
2024-01-26 11:19:42 +0900

    [ADD] 지역 재난 알림 조회 컨트롤러
    요청 파라미터들(offset, size, startDate, endDate) 검증 처리
    본인 소유 매장 점주만 접근하도록 권한 제한

---
+++ src/main/java/com/ssg/usms/business/warning/controller/RegionWarningController.java
@@ -0,0 +1,57 @@
+package com.ssg.usms.business.warning.controller;
+
+import com.ssg.usms.business.accident.exception.InvalidDateFlowException;
+import com.ssg.usms.business.security.login.UsmsUserDetails;
+import com.ssg.usms.business.store.exception.UnavailableStoreException;
+import com.ssg.usms.business.store.service.StoreService;
+import com.ssg.usms.business.warning.dto.HttpRequestRetrievingRegionWarningDto;
+import com.ssg.usms.business.warning.dto.RegionWarningDto;
+import com.ssg.usms.business.warning.service.RegionWarningService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class RegionWarningController {
+
+    private final StoreService storeService;
+    private final RegionWarningService regionWarningService;
+
+    @GetMapping("/api/users/{userId}/stores/{storeId}/accidents/region")
+    public List<RegionWarningDto> findAllByStoreId(@PathVariable Long userId,
+                                                   @PathVariable Long storeId,
+                                                   @ModelAttribute @Valid HttpRequestRetrievingRegionWarningDto requestParams) {
+
+        validateDateFlow(requestParams.getStartDate(), requestParams.getEndDate());
+
+        userId = ((UsmsUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getId();
+        storeService.validateOwnedStore(storeId, userId);
+        if(!storeService.isAvailable(storeId)) {
+            throw new UnavailableStoreException();
+        }
+
+        return regionWarningService.findByRegion(storeId,
+                                                requestParams.getStartDate(),
+                                                requestParams.getEndDate(),
+                                                requestParams.getOffset(),
+                                                requestParams.getSize());
+    }
+
+    private void validateDateFlow(String startDate, String endDate) {
+
+        if(startDate == null || endDate == null) {
+            return;
+        }
+        if(LocalDate.parse(startDate).isAfter(LocalDate.parse(endDate))) {
+            throw new InvalidDateFlowException();
+        }
+    }
+}
