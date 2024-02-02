e9b04a571ef7e874c4cea04b985b491f798bb563
aksenaksen <ans109905@naver.com>
2024-02-02 13:03:13 +0900

    [FIX] 유저관련 업데이트

--- src/main/java/com/ssg/usms/business/user/constant/UserConstants.java
+++ src/main/java/com/ssg/usms/business/user/constant/UserConstants.java
@@ -2,7 +2,7 @@ package com.ssg.usms.business.user.constant;
 
 public class UserConstants {
     public static final String EMAIL_PATTERN = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
-    public static final String NICKNAME_PATTERN = "[a-zA-Z0-9]{1,10}";
+    public static final String NICKNAME_PATTERN = "[a-zA-Z가-힣0-9\\s]{1,10}";
     public static final String PASSWORD_PATTERN = "^(?=.*[A-Za-z])(?=.*[@#$%^&+=!*])(?=.*\\d).{8,30}$";
     public static final String USERNAME_PATTERN = "[a-zA-Z0-9]{5,20}";
     public static final String PHONENUMBER_PATTERN = "^010-\\d{4}-\\d{4}";
